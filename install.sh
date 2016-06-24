8c8e548f016dfd9b9d9815c1570e3381c5b0ad64
Lee McKinnon <leemac314@gmail.com>
2016-06-23 20:30:24 -0400

    adding requirement for python-pillow

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ echo "Updating Repository"
 sudo apt-get update
 
 echo "Installing apt packages..."
-apt-get -y install git python3-pip postgresql postgresql-contrib pgadmin3 atom build-essential tmux libpq-dev python3-dev
+apt-get -y install git python3-pip postgresql postgresql-contrib pgadmin3 atom build-essential tmux libpq-dev python3-dev libjpeg-dev
 
 echo "Installing pip packages"
 pip3 install virtualenvwrapper
